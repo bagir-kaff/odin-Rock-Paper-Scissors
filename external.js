ecd240c93b8146ccccc53695ba0276159d819c1c
bagir-kaff <trexhilang@gmail.com>
2023-08-16 17:51:27 +0330

    Rework functions
    i changed hchoices so that i can add other button,
    added result and cs.textContent to update the
    score and to show who won, change function play.
    get attribute from data-choice

--- external.js
+++ external.js
@@ -1,39 +1,64 @@
-const choices = ['Rock','Paper','Scissors']
-getComputerChoice = () => choices[Math.floor(Math.random()*3 +1)] //FUNCTION
-let comp = getComputerChoice();
-let human;
+const choices = ['Rock','Paper','Scissors'];
+getComputerChoice = () => choices[Math.floor(Math.random()*3 )]; //FUNCTION
+let comp,
+human,
+compScore = 0,
+humanScore = 0;
+const result = document.querySelector('.result');
+const hs = document.querySelector('.human');
+const cs = document.querySelector('.comp'); 
+const hchoices = document.querySelectorAll('.btn');
 
-function play(cc,hc) { //cc = computer choice,hc = human choice
-  hc = hc[0].toUpperCase() + hc.slice(1, hc.length).toLowerCase()
-  human = hc;
-  if (cc === 'Rock' && hc === 'Paper' || cc === 'Paper' && hc === 'Scissors' ||cc === 'Scissors' && hc === 'Rock' )
-    return 1;
-
-  else if (cc === 'Paper' && hc === 'Rock' || cc === 'Scissors' && hc === 'Paper' ||cc === 'Rock' && hc === 'Scissors' )
-    return -1
-
-  else
-    return 0
-   
+function play(){
+  if(compScore <5 && humanScore <5){
+  comp = getComputerChoice();
+  human = this.getAttribute('data-choice');
+  win(comp,human);
+  console.log()
+  }
 }
+hchoices.forEach(ch => {
+  ch.addEventListener('click',play);
+});
+
+function win(cc,hc) { //cc = computer choice,hc = human choice
+  if (cc === 'Rock' && hc === 'Paper' || cc === 'Paper' && hc === 'Scissors' ||cc === 'Scissors' && hc === 'Rock' ){
+    hs.textContent = `Human: ${++humanScore}`;
+    result.textContent = `You Won This Round, ${hc} beats ${cc}`;
+  }
+  else if (cc === 'Paper' && hc === 'Rock' || cc === 'Scissors' && hc === 'Paper' ||cc === 'Rock' && hc === 'Scissors' ){
+    cs.textContent = `Computer: ${++compScore}`;
+    result.textContent = `You Lose This Round, ${cc} beats ${hc}`;
+  }
+  else if ((cc === 'Paper' && hc === 'Paper' || cc === 'Scissors' && hc === 'Scissors' ||cc === 'Rock' && hc === 'Rock' ))
+    result.textContent = `Its A Tie, ${cc} ties with ${hc}`;
+  else
+    result.textContent =`something is wrong (testing only)`
+}
+
 function game(){
     let score = 0;
     let res;
-    for(let i =0;i<5;i++){
-        res = play(getComputerChoice(), prompt("input your choice"))
+        // human = prompt("input your choice");
+        // human = human[0].toUpperCase() + human.slice(1, human.length).toLowerCase()
+        // if ((human === 'Rock' ||human === 'Scissors' ||human === 'Paper' )){break}
+        // alert("wrong input!")
+    // }
+        res = play(getComputerChoice(),human)
         score += res;
         if(res === 1) console.log(`You Won This Round, ${human} beats ${comp}`)
         else if (res === 0) console.log(`Its A Tie, ${comp} ties with ${human}`)
         else if (res === -1) console.log(`You Lose This Round, ${comp} beats ${human}`)
-    }
+    
     if (score>0){
-        alert( "Yes You won! Againts Randomness")
+        // alert( "Yes You won! Againts Randomness")
     }
     else if (score === 0){
-        alert( "you tied with randomness")
+        // alert( "you tied with randomness")
     }
-    else
-    alert( "you Lose againts randomness")
-}
+    else{
+    // alert( "you Lose againts randomness")
+    }
+  }
 
-game()
+// game()
